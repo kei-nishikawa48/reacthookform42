fa8c5b8a7a26271f3a9154987edf0ed1aa91e9c4
kei <xnkcelebrate48x@yahoo.co.jp>
2020-12-20 20:04:58 +0900

    reacthookform練習

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react"
+import { useForm } from "react-hook-form" //react-hook-formからuseFormをインポート
 
-function App() {
+const App = () => {
+  const {register,handleSubmit,errors} =useForm()　//useForm()から register(登録) handleSubmit(送信するときの関数) 
+  // const [name, setName] = useState("")
+  // const [email, setEmail] = useState("")
+  // const [password, setPassword] = useState("")
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   console.log(name, email, password)
+  // }
+  const signUp=(data)=>{
+    console.log(data)
+    // data={name:"",email:"",password:""}
+  } //onSubmitした時に呼ばれる関数
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(signUp)}>
+      <label>name</label>
+      <input 
+      name="name"  //react-hook-formで他のinputタグと判別するために付ける
+      type="text"
+      ref={register({
+        required:"入力してください",
+      })} //インプットタグをreact-hook-formに登録する　　　requiredというのは必須という意味
+      //  onChange={(e) => setName(e.target.value)} 
+      />
+      {errors.name && errors.name.message}
+      <label>email</label>
+      <input 
+      name="email"
+      type="text" 
+      ref={register}
+      // onChange={(e) => setEmail(e.target.value)} 
+      />
+      <label>password</label>
+      <input 
+      name="password"
+      type="password" 
+      ref={register(
+        {
+          required:"入力してください",
+          minLength:{
+            value:6,
+            message:"パスワードは６文字以上です"
+          }
+        }
+      )}
+      // onChange={(e) => setPassword(e.target.value)} 
+      />
+      {errors.password&&errors.password.message}
+      <button type="submit">SignUp</button>
+    </form>
   );
 }
 
